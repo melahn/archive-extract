53e7bb7c4ba11b09bebc50e26bf85558c6a4544b
melahn <gregorymelahn@gmail.com>
2021-04-12 16:16:50 -0400

    updated unzipEmbeddedZips after testing

--- src/main/java/com/melahn/util/zip/ZipSlipExample.java
+++ src/main/java/com/melahn/util/zip/ZipSlipExample.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,6 +25,8 @@ import java.util.stream.Stream;
 public class ZipSlipExample {
 
     private static final Logger logger = LogManager.getLogger("ZipSlipExample");
+    private static final String SEPARATOR = FileSystems.getDefault().getSeparator();
+
     /**
      * Using test.tgz or the name supplied in args[0], unzip the tgz without
      * checking for the zip slip issue.
@@ -44,8 +47,8 @@ public class ZipSlipExample {
     }
 
     /**
-     * Induce the Zip Slip security issue by not checking an entry in the file crossing the boundary of the 
-     * zip file directory
+     * Induce the Zip Slip security issue by not checking an entry in the file
+     * crossing the boundary of the zip file directory
      * 
      * @param z The name of a tgz file
      * @param t The directory in which to unzip the file
@@ -59,27 +62,40 @@ public class ZipSlipExample {
             TarArchiveEntry entry;
             while ((entry = (TarArchiveEntry) tis.getNextEntry()) != null) {
                 String name = entry.getName();
+                // protect against hidden files and relative names
+                if (name.contains(SEPARATOR) && name.lastIndexOf(SEPARATOR) != name.length() - 1
+                        && (name.substring(name.lastIndexOf(SEPARATOR) + 1, name.length())).startsWith(".")) {
+                    logger.info("Entry {} skipped", name);
+                    continue;
+                }
                 Path fileToCreate = t.resolve(name);
                 final int bufferSize = 1024;
                 byte[] data = new byte[bufferSize];
-                // first create the parent directory if it does not exist
+                // Note the order in which the entries appear is not predictable
                 Path parent = fileToCreate.getParent();
-                if (Files.notExists(parent)) {
+                if (Files.notExists(parent)) { // first create the parent directory if it does not exist
                     Files.createDirectories(parent);
                     logger.info("Directory {} created", parent);
                 }
-                Path newFile = Files.createFile(fileToCreate);
-                logger.info("File {} created", fileToCreate);
-                while ((tis.read(data, 0, bufferSize)) != -1) {
-                    Files.write(newFile, data, StandardOpenOption.APPEND);
+                if (entry.isDirectory() && Files.notExists(fileToCreate)) { // create the directory
+                    Files.createDirectory(fileToCreate);
+                    logger.info("Directory {} created", fileToCreate);
+                } else if (Files.notExists(fileToCreate)) { // create the file
+                    Path newFile = Files.createFile(fileToCreate);
+                    logger.info("File {} created", fileToCreate);
+                    while ((tis.read(data, 0, bufferSize)) != -1) {
+                        Files.write(newFile, data, StandardOpenOption.APPEND);
+                    }
+                    logger.info("File {} created", fileToCreate);
                 }
-                logger.info("File {} created", fileToCreate);
             }
+            logger.info("File {} unzipped", z);
         } catch (IOException e) {
             logger.error("IO Exception unzipping {}", z);
             throw e;
         }
     }
+
     /**
      * create a directory in which to unzip the file
      * 
@@ -100,24 +116,22 @@ public class ZipSlipExample {
         }
     }
 
-     /**
+    /**
      * Unpacks any tgz files found in a directory
      *
      * @param d the name of the directory in which to look
      */
-      private void unzipEmbeddedZips (Path d) throws IOException {
-        try (Stream<Path> w = Files.walk(d,10)) {
+    private void unzipEmbeddedZips(Path d) throws IOException {
+        try (Stream<Path> w = Files.walk(d, 10)) {
             List<Path> tgzFiles = w.filter(Files::isRegularFile)
-            .filter(p -> p.getFileName().toString().endsWith(".tgz"))
-            .collect(Collectors.toList());
+                    .filter(p -> p.getFileName().toString().endsWith(".tgz")).collect(Collectors.toList());
             for (Path z : tgzFiles) {
-                logger.info("tgz file: {} found" , z); 
-                unzip(z.getFileName().toString(),z.getParent());
+                Path r = d.resolve(z);
+                logger.info("tgz file: {} found", r);
+                unzip(r.toString(), r.getParent());
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             logger.error("IOException looking for embedded zip files");
         }
-        
-      }
+    }
 }
