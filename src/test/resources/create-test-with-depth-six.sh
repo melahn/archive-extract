47e93c8797e2cec9e883539e6654b8933e3c24f6
melahn <gregorymelahn@gmail.com>
2021-05-01 16:02:17 -0400

    Improved the nested tgz test

--- src/test/resources/create-test-with-depth-six.sh
+++ src/test/resources/create-test-with-depth-six.sh
@@ -1,26 +1,18 @@
 # create a compressed archive that has a depth of six
 # compressed archive members
 mkdir -p test-with-depth-six/A/B/C/D/E/F
-cd test-with-depth-six/A/B/C/D/E/F
-echo foo > foo
-cd ..
-tar -cvzf F.tgz --exclude ".DS_Store" F/foo
-rm -rf F
-cd ..
-tar -cvzf E.tgz --exclude ".DS_Store" E/*
-cd ..
-rm -rf E
-tar -cvzf D.tgz --exclude ".DS_Store" D/*
-cd ..
-rm -rf D
-tar -cvzf C.tgz --exclude ".DS_Store" C/*
-cd ..
-rm -rf C
-tar -cvzf B.tgz --exclude ".DS_Store" B/*
-cd ..
-rm -rf B
-tar -cvzf A.tgz --exclude ".DS_Store" A/*
-cd ..
-rm -rf A
-tar -cvzf test-with-depth-six.tgz --exclude ".DS_Store" test-with-depth-six/*
+echo foo > test-with-depth-six/A/B/C/D/E/F/foo
+tar -cvzf test-with-depth-six/A/B/C/D/E/F.tgz -C test-with-depth-six/A/B/C/D/E  --exclude ".DS_Store" F/foo
+rm -rf test-with-depth-six/A/B/C/D/E/F
+tar -cvzf test-with-depth-six/A/B/C/D/E.tgz -C test-with-depth-six/A/B/C/D --exclude ".DS_Store" E/F.tgz
+rm -rf test-with-depth-six/A/B/C/D/E
+tar -cvzf test-with-depth-six/A/B/C/D.tgz -C test-with-depth-six/A/B/C --exclude ".DS_Store" D/E.tgz
+rm -rf test-with-depth-six/A/B/C/D
+tar -cvzf test-with-depth-six/A/B/C.tgz -C test-with-depth-six/A/B --exclude ".DS_Store" C/D.tgz
+rm -rf test-with-depth-six/A/B/C
+tar -cvzf test-with-depth-six/A/B.tgz -C test-with-depth-six/A --exclude ".DS_Store" B/C.tgz
+rm -rf test-with-depth-six/A/B
+tar -cvzf test-with-depth-six/A.tgz -C test-with-depth-six --exclude ".DS_Store" A/B.tgz
+rm -rf test-with-depth-six/A
+tar -cvzf test-with-depth-six.tgz --exclude ".DS_Store" test-with-depth-six/A.tgz
 rm -rf test-with-depth-six
